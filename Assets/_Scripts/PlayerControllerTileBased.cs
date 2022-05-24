9c3b25101fa2adf757d255f0346c8ae4a400865b
Stephano Naressi <nanbatsu@outlook.com>
2022-05-24 20:54:30 +0100

    Working on better offbounds

--- Assets/_Scripts/PlayerControllerTileBased.cs
+++ Assets/_Scripts/PlayerControllerTileBased.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class PlayerControllerTileBased : MonoBehaviour
 {
+    [SerializeField]
+    SpriteRenderer spr;
+    [SerializeField]
+    Sprite[] sprImg;
     Vector3 playerOldPos;
+    [SerializeField]
+    GameObject ColCheck;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,17 +23,25 @@ public class PlayerControllerTileBased : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             moveTile(Vector3.up);
+            ColCheck.transform.position = gameObject.transform.position +  Vector3.up;
+            spr.sprite = sprImg[3];
         }
         else if(Input.GetKeyDown(KeyCode.DownArrow))
         {
             moveTile(Vector3.down);
+            ColCheck.transform.position = gameObject.transform.position +  Vector3.down;
+            spr.sprite = sprImg[2];
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            spr.sprite = sprImg[0];
+            ColCheck.transform.position = gameObject.transform.position + Vector3.left;
             moveTile(Vector3.left);
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
+            spr.sprite = sprImg[1];
+            ColCheck.transform.position = gameObject.transform.position + Vector3.right;
             moveTile(Vector3.right);
         }
 
