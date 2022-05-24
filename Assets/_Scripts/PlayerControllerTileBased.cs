7560cd988901fe95a24aec875d96000d02f1c52b
Stephano Naressi <nanbatsu@outlook.com>
2022-05-24 23:19:52 +0100

    Better Player Controller

--- Assets/_Scripts/PlayerControllerTileBased.cs
+++ Assets/_Scripts/PlayerControllerTileBased.cs
@@ -8,85 +8,83 @@ public class PlayerControllerTileBased : MonoBehaviour
     SpriteRenderer spr;
     [SerializeField]
     Sprite[] sprImg;
-    Vector3 playerOldPos;
+    //Vector3 playerOldPos;
     [SerializeField]
-    GameObject ColCheck;
-    public bool canMove;
-    Vector3 lookingDir;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    float rayDist;
+    public Vector3 lookingDir;
     // Update is called once per frame
     void Update()
     {
-        canMove = GetComponentInChildren<ChildCollider>().canMove;
-        if (canMove)
-        {
             if (Input.GetKeyDown(KeyCode.UpArrow))
             {
                 lookingDir = Vector3.up;
-                moveTile(Vector3.up);
-                //ColCheck.transform.position = gameObject.transform.position + Vector3.up;
+                raycastCheck(Vector3.up);
                 spr.sprite = sprImg[3];
             }
             else if (Input.GetKeyDown(KeyCode.DownArrow))
             {
                 lookingDir = Vector3.down;
-                moveTile(Vector3.down);
-                //ColCheck.transform.position = gameObject.transform.position + Vector3.down;
+                raycastCheck(Vector3.down);
                 spr.sprite = sprImg[2];
             }
             else if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
                 lookingDir = Vector3.left;
+                raycastCheck(Vector3.left);
                 spr.sprite = sprImg[0];
-                //ColCheck.transform.position = gameObject.transform.position + Vector3.left;
-                moveTile(Vector3.left);
             }
             else if (Input.GetKeyDown(KeyCode.RightArrow))
             {
                 lookingDir = Vector3.right;
+                raycastCheck(Vector3.right);
                 spr.sprite = sprImg[1];
-               // ColCheck.transform.position = gameObject.transform.position + Vector3.right;
-                moveTile(Vector3.right);
+            }
+    }
+    void raycastCheck(Vector3 v)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position+v, transform.TransformDirection(v), 0.2f);
+        //Debug.DrawRay(gameObject.transform.position+v, transform.TransformDirection(-v)*1f, Color.red,1f);
+        if(hit.collider != null)
+        {
+            if (hit.collider.tag == "Collider")
+            {
+               // print(hit.collider.name);
+            }
+            else if (hit.collider.tag == "pushableObject")
+            {
+                switch (lookingDir)
+                {
+                    //looking up
+                    case Vector3 z when z.Equals(Vector3.up):
+                        print("ASD");
+                        hit.collider.transform.Translate(Vector3.up, Space.World);
+                        break;
+                    //looking down
+                    case Vector3 z when z.Equals(Vector3.down):
+                        hit.collider.transform.Translate(Vector3.down, Space.World);
+                        break;
+                    //looking left
+                    case Vector3 z when z.Equals(Vector3.left):
+                        hit.collider.transform.Translate(Vector3.left, Space.World);
+                        break;
+                    //looking right
+                    case Vector3 z when z.Equals(Vector3.right):
+                        hit.collider.transform.Translate(Vector3.right, Space.World);
+                        break;
+                
+                }
+                
             }
         }
         else
         {
-            switch (lookingDir)
-            {
-                //looking up
-                case Vector3 v when v.Equals(Vector3.up):
-                    moveTile(Vector3.down);
-                    GetComponentInChildren<ChildCollider>().canMove = true;
-                    break;
-                //looking down
-                case Vector3 v when v.Equals(Vector3.down):
-                    moveTile(Vector3.up);
-                    GetComponentInChildren<ChildCollider>().canMove = true;
-                    break;
-                //looking left
-                case Vector3 v when v.Equals(Vector3.left):
-                    moveTile(Vector3.right);
-                    GetComponentInChildren<ChildCollider>().canMove = true;
-                    break;
-                //looking right
-                case Vector3 v when v.Equals(Vector3.right):
-                    moveTile(Vector3.left);
-                    GetComponentInChildren<ChildCollider>().canMove = true;
-                    break;
-            }
+            moveTile(v);
         }
-
 
     }
     private void moveTile(Vector3 v)
     {
-        playerOldPos = gameObject.transform.position;
-       
-        gameObject.transform.Translate(v , Space.World);
+        //playerOldPos = gameObject.transform.position;
+        gameObject.transform.Translate(v, Space.World);
     }
 }
